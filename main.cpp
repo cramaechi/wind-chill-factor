2381362cd4f125fac05de164e644f1e6e9a619f0
Chibuikem Amaechi <chibuikem_amaechi@Chibuikems-MacBook-Pro.local>
2017-12-08 12:07:58 -0600

    Modified .main.cpp.swp and main.cpp

--- main.cpp
+++ main.cpp
@@ -11,44 +11,44 @@ double computeWCF(double v, double t);
 
 int main(int argc, char* argv[])
 {
-	char ans;
-	double v, t;
+    char ans;
+    double v, t;
 
-	do
-	{
-	   cout<<"Enter today's temperature (in degrees Celsius <= 10) and wind speed (in m/sec): ";
-	   cin>>t;
-	   cin>>v;
-	   cout.setf(ios::showpoint);
-	   cout.setf(ios::fixed);
-	   cout.precision(2);
-	   cout<<"The wind chill factor for today is: "<< computeWCF(v, t)<<"°C\n\nStart over? (y/n) ";
-	   cin>>ans;
-	    
-	   if(ans == 'y' || ans == 'Y')
-		   cout<<"\n\n";
-	}while(ans == 'y' || ans == 'Y');
+    do
+    {
+        cout<<"Enter today's temperature (in degrees Celsius <= 10) and wind speed (in m/sec): ";
+        cin>>t;
+        cin>>v;
+        cout.setf(ios::showpoint);
+        cout.setf(ios::fixed);
+        cout.precision(2);
+        cout<<"The wind chill factor for today is: "<< computeWCF(v, t)<<"°C\n\nStart over? (y/n) ";
+        cin>>ans;
 
-	system ("PAUSE");
-	return 0;
+        if(ans == 'y' || ans == 'Y')
+            cout<<"\n\n";
+    }while(ans == 'y' || ans == 'Y');
+
+    system ("PAUSE");
+    return 0;
 }
 
 double computeWCF(double v, double t)
 {
-	if (t > 10)
-	{
-		cout<<"Fatal error: Aborting program.";
-		exit(1);
-	}
+    if (t > 10)
+    {
+        cout<<"Fatal error: Aborting program.";
+        exit(1);
+    }
 
-	double w = 33.0 - (((10*sqrt(v)) - v + 10.5)*(33.0 - t))/23.1;
-	
-	return w;
+    double w = 33.0 - (((10*sqrt(v)) - v + 10.5)*(33.0 - t))/23.1;
+
+    return w;
 }
 
 /*
-void clearScreen()
-{
+   void clearScreen()
+   {
    cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-}
-*/
+   }
+   */
